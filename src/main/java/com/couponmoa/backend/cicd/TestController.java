9e6278d045ea9fcf574da96918ea985f0b3cb06e
나연 <kny5579@naver.com>
2025-04-04 16:18:23 +0900

    feat: test CI

--- src/main/java/com/couponmoa/backend/cicd/TestController.java
+++ src/main/java/com/couponmoa/backend/cicd/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
     @GetMapping("/")
     public String home() {
-        return "CICD Docker version: " + version;
+        return "test CICD Docker version: " + version;
     }
 
 }
